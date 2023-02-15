defd05de8a7759a5286822731bbf7592271a8fdf
jackson <jackson@clobot.co.kr>
2023-02-15 19:04:00 +0900

    <feat> Add Manager Class to EmployeeList
    EmployeeList에 Manager Class 추가 및 파일 위치 이동

--- Jackson/MoDooCode/cpp/6-1/employee_wo_inheritance/employeelist.cpp
+++ Jackson/MoDooCode/cpp/6-1/employee_wo_inheritance/employeelist.cpp
@@ -3,7 +3,9 @@
 EmployeeList::EmployeeList(int alloc_employee)
     : alloc_employee_(alloc_employee) {
   employee_list_ = new Employee *[alloc_employee_];
+  manager_list_ = new Manager *[alloc_employee_];
   current_employee_ = 0;
+  current_manager_ = 0;
 };
 
 void EmployeeList::add_employee(Employee *em) {
@@ -13,8 +15,19 @@ void EmployeeList::add_employee(Employee *em) {
   employee_list_[current_employee_] = em;
   ++current_employee_;
 }
+void EmployeeList::add_manager(Manager *ma) {
+  if (alloc_employee_ == current_manager_) {
+    reserve(2 * alloc_employee_);
+  }
+  manager_list_[current_manager_] = ma;
+  ++current_manager_;
+}
 
 int EmployeeList::current_employee() { return current_employee_; }
+int EmployeeList::current_manager() { return current_manager_; }
+int EmployeeList::total_employee() {
+  return current_employee_ + current_manager_;
+}
 
 void EmployeeList::print_employee_info() {
   int total_pay = 0;
@@ -22,15 +35,28 @@ void EmployeeList::print_employee_info() {
     employee_list_[i]->print_info();
     total_pay += employee_list_[i]->salary();
   }
+  for (int i = 0; i < current_manager_; ++i) {
+    manager_list_[i]->print_info();
+    total_pay += manager_list_[i]->salary();
+  }
+
   std::cout << "Total pay --> " << total_pay << std::endl;
 }
 
 void EmployeeList::reserve(int alloc_employee) {
-  auto tmp = employee_list_;
+  auto em_tmp = employee_list_;
   employee_list_ = new Employee *[alloc_employee];
   for (int i = 0; i < current_employee_; ++i) {
-    employee_list_[i] = tmp[i];
+    employee_list_[i] = em_tmp[i];
   }
+  delete[] em_tmp;
+
+  auto ma_tmp = manager_list_;
+  manager_list_ = new Manager *[alloc_employee];
+  for (int i = 0; i < current_manager_; ++i) {
+    manager_list_[i] = ma_tmp[i];
+  }
+  delete[] ma_tmp;
   alloc_employee_ = alloc_employee;
 }
 
@@ -39,4 +65,9 @@ EmployeeList::~EmployeeList() {
     delete employee_list_[i];
   }
   delete[] employee_list_;
+
+  for (int i = 0; i < current_manager_; ++i) {
+    delete manager_list_[i];
+  }
+  delete[] manager_list_;
 }
