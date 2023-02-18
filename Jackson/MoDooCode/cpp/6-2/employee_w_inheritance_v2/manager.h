47f0b9d753baec16810ff6fc33be2ca6855df86d
jackson <jackson@clobot.co.kr>
2023-02-18 17:27:00 +0900

    <feat> Modify EmployeeList with Inheritance and UpCasting
    Upcasting까지 적용하여 코드 정리 진행

---
+++ Jackson/MoDooCode/cpp/6-2/employee_w_inheritance_v2/manager.h
@@ -0,0 +1,20 @@
+#ifndef D651513F_89CB_4E1A_9DBF_B073A25F1AA1
+#define D651513F_89CB_4E1A_9DBF_B073A25F1AA1
+
+#include <iostream>
+#include <string>
+#include <employee.h>
+
+class Manager : public Employee{
+private:
+  int year_of_service_; // 근속 연수
+
+public:
+  Manager(std::string name, int age, std::string position, int rank, int year_of_service);
+  Manager(const Manager &manager);
+  Manager() = default;
+  void print_info();
+  int salary();
+};
+
+#endif /* D651513F_89CB_4E1A_9DBF_B073A25F1AA1 */
